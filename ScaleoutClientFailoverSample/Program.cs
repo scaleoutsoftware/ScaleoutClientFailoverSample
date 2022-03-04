77b42d34a846cb45098638bad0cdabe49e56068c
Mark Waterman <markwaterman@users.noreply.github.com>
2022-03-03 16:31:57 -0800

    Clean up PollyClient example usage in Main().

--- ScaleoutClientFailoverSample/Program.cs
+++ ScaleoutClientFailoverSample/Program.cs
@@ -11,15 +11,19 @@ namespace ScaleoutClientFailoverSample
     public class Program
     {
         private static readonly string CacheName = "MyCache";
+
+        // Note that maxRequestRetries is set to zero in connection strings so failover is faster.
+        // See https://static.scaleoutsoftware.com/docs/dotnet_client/articles/configuration/connecting.html
+
+        private static readonly string PrimaryConnString = "bootstrapGateways=192.168.1.107:721;maxRequestRetries=0";
+        private static readonly string FailoverConnString = "bootstrapGateways=192.168.1.60:721;maxRequestRetries=0";
+
         public static void Main()
         {
-            // Note that maxRequestRetries is set to zero in connection strings so failover is faster.
-            // See https://static.scaleoutsoftware.com/docs/dotnet_client/articles/configuration/connecting.html
-
             var pollyClient = new PollyClient<string, string>(
                 primaryCacheFactory: () =>
                 {
-                    var primaryConn = GridConnection.Connect("bootstrapGateways=192.168.1.107:721;maxRequestRetries=0");
+                    var primaryConn = GridConnection.Connect(PrimaryConnString);
                     var primaryCache = new CacheBuilder<string, string>(CacheName, primaryConn)
                                             .Build();
                     return primaryCache;
@@ -27,33 +31,20 @@ namespace ScaleoutClientFailoverSample
 
                 failoverCacheFactory: () =>
                 {
-                    var failoverConn = GridConnection.Connect("bootstrapGateways=192.168.1.60:721;maxRequestRetries=0");
+                    var failoverConn = GridConnection.Connect(FailoverConnString);
                     var failoverCache = new CacheBuilder<string, string>(CacheName, failoverConn)
                                             .Build();
                     return failoverCache;
                 },
 
-                failoverDuration: TimeSpan.FromSeconds(20)
+                failoverDuration: TimeSpan.FromSeconds(20) // how long to wait before retrying primary after a failure
             );
 
+
+            // Wrap cache accesses in a DoScaleoutRequest() request call if failover is needed.
             var response = pollyClient.DoScaleoutRequest(cache => cache.Add("key1", "value1"));
+            response = pollyClient.DoScaleoutRequest(cache => cache.Read("key1"));
 
-            Console.WriteLine(response.Result);
-
-            while (true)
-            {
-                try
-                {
-                    Console.ReadLine();
-                    response = pollyClient.DoScaleoutRequest(cache => cache.ReadOrAdd("key1", () => "whee!"));
-                    Console.WriteLine(response.Result);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                
-            }
         }
 
     }
